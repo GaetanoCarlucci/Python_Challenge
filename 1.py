6c8209856f2a39ab8a74f96d12db094b69aab349
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2016-09-06 20:34:03 +0200

    first solution

---
+++ 1.py
@@ -0,0 +1,13 @@
+import string 
+
+message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+intab =  string.ascii_lowercase
+outtab = intab[2:] + intab[:2]
+trantab = string.maketrans(intab, outtab)
+
+print message.translate(trantab)
+
+link = "map"
+
+print "The link is: " + link.translate(trantab) + ".html"
---
+++ README.md
@@ -0,0 +1,13 @@
+# Python Challenge Solutions
+[Python Challenge](http://www.pythonchallenge.com/) is a game in which each level can be solved by a bit of Python programming. 
+
+Here I provide my solutions.
+
+## Description
+
+Each file name corresponds to the number of the riddle.
+
+For exampke to run the solution for riddle n 1 type in command line:
+```sh
+python 1.py
+```
