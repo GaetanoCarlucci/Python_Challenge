e3b301641d310dfddca8de7458523eae9015a93a
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2016-09-06 21:17:12 +0200

    adding solution 3

---
+++ 3.py
@@ -0,0 +1,6 @@
+import urllib
+import re
+
+page = urllib.urlopen('http://www.pythonchallenge.com/pc/def/equality.html').readlines()[21:]
+print "The link is " + "".join(re.findall(r"[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]", "".join(page))) + ".html"
+
