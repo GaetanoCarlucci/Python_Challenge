2760c65cd3b99f7d34ff24fb7a81f33e2fb8e7c8
Gaetano Carlucci <gaetano.carlucci@gmail.com>
2017-04-11 17:52:43 +0200

    Update 15.py

--- 15.py
+++ 15.py
@@ -2,7 +2,7 @@ from calendar import weekday, isleap
 
 candidate_years = [i for i in range(1006, 2006, 10) if (weekday(i, 1, 1) == 3) and isleap(i)]
 
-#we should take the not the youngest, but the second.
+#we should not take the youngest, but the second.
 print "The date should be 27 January " + str(candidate_years[-2])
 
 print "It seems that Mozart has born this day."
